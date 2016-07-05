0dce14d21484dec0147ef7b12739def68b2bf326
Patrick Smith <pgwsmith@gmail.com>
2016-07-06 00:56:48 +1000

    Make type group fields pills

--- src/Meadow.js
+++ src/Meadow.js
@@ -1,12 +1,19 @@
+import R from 'ramda'
 import React from 'react'
 import Meadow from 'react-meadow'
 import webComponents from 'react-meadow/lib/defaultComponents'
 import * as stylers from './stylers/meadow'
 
+const keyPathIsTransformTypeGroup = R.pipe(
+	R.slice(-2, -1),
+	R.equals(['transforms'])
+)
+
 const Field = webComponents.fieldComponent
 const StyledField = (props) => (
 	<Field
 		{ ...props }
+		pills={ keyPathIsTransformTypeGroup(props.keyPath) }
 		labelStyler={ stylers.label }
 		inputStyler={ stylers.input }
 	/>
